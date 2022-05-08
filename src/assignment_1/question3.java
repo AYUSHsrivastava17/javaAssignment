e0cb03d13a39a44f7ce7f492738e27ae652a223f
AYUSHsrivastava17 <ayush150923@gmail.com>
2022-05-08 21:12:22 +0530

    question3

---
+++ src/assignment_1/question3.java
@@ -0,0 +1,37 @@
+package assignment_1;
+
+public class question3 {
+
+        public static void main(String[] args) {
+
+            testClass obj = new testClass();
+            obj.display(55);
+        }
+    }
+
+    interface In1{
+
+        void display(int p);
+    }
+
+    class testClass implements In1{
+
+        public void display( int p ) {
+
+            int c = 0;
+            for( int i=2 ; i<=Math.sqrt(p) ; i++ ) {
+                if( p%i == 0 ) {
+                    c++;
+                    break;
+                }
+            }
+            if( c == 0 ) {
+                System.out.println(p+" is a prime number.");
+            }
+            else {
+                System.out.println(p+" is not a prime number.");
+            }
+            return;
+        }
+    }
+
